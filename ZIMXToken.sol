73eaf2e851f50037848352c1b354bfc996a58423
roy-bme <roym.bme@gmail.com>
2025-08-08 19:47:42 +0100

    feat: add permit

--- ZIMXToken.sol
+++ ZIMXToken.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ZIMXToken
  * @notice ZIMX utility token with fixed supply and emergency pause.
  */
-contract ZIMXToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+contract ZIMXToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Permit, Ownable {
     /// @notice Address receiving the initial token supply and holding treasury funds.
     address public treasury;
 
@@ -19,11 +20,13 @@ contract ZIMXToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 
     /**
      * @notice Deploys the ZIMX token, assigns the initial owner, and mints the full supply to the treasury.
+     * @dev Initializes {ERC20Permit} for EIP-2612 support.
      * @param initialOwner Address that will be granted ownership of the contract.
      * @param treasury_ Address of the treasury wallet that receives the full token supply.
      */
     constructor(address initialOwner, address treasury_)
         ERC20("ZIMX Token", "ZIMX")
+        ERC20Permit("ZIMX Token")
         Ownable(initialOwner)
     {
         require(initialOwner != address(0), "Owner address is zero");
@@ -41,6 +44,14 @@ contract ZIMXToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
      */
     function decimals() public pure override returns (uint8) {
         return 6;
+    }
+
+    /**
+     * @notice Returns the total supply of tokens.
+     * @dev Helper alias for {totalSupply}.
+     */
+    function totalSupplyRaw() external view returns (uint256) {
+        return totalSupply();
     }
 
     /**
